1d73b291eaf2b4803eddc7f13f79f4cc71110288
Ahmed Oueslati <weslaty@gmail.com>
2020-01-19 19:29:53 +0100

    Gérer les évévenements pour plusieurs champs de saisie

--- src/forms/NameForm.js
+++ src/forms/NameForm.js
@@ -3,15 +3,17 @@ import React from 'react'
 export default class NameForm extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { value: 'KARAKOUZ' , txt: 'MDA3MECH'}
+        this.state = { name: 'KARAKOUZ', description: 'MDA3MECH' }
     }
 
     handleChange = (event) => {
-        this.setState({ value: event.target.value.toUpperCase() });
+        const inputName = event.target.name
+        const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value.toUpperCase()
+        this.setState({ [inputName]: value })
     }
 
     handleSubmit = (event) => {
-        alert('Le nom a été soumis : ' + this.state.value)
+        alert('Le nom a été soumis : ' + JSON.stringify(this.state))
         event.preventDefault()
     }
 
@@ -20,8 +22,19 @@ export default class NameForm extends React.Component {
             <form onSubmit={this.handleSubmit}>
                 <label>
                     Nom :
-                    <input value={this.state.value} onChange={this.handleChange} />
-                    <textarea value={this.state.value} onChange={this.handleChange} />
+                    <input name="name" value={this.state.name} onChange={this.handleChange} />
+                </label>
+                <label>
+                    Nom :
+                    <input name="prenom" value={this.state.prenom} onChange={this.handleChange} />
+                </label>
+                <label>
+                    Description :
+                    <textarea name="description" value={this.state.description} onChange={this.handleChange} />
+                </label>
+                <label>
+                    Participe :
+                    <input type="checkbox" name="participe" value={this.state.participe} onChange={this.handleChange} />
                 </label>
                 <input type="submit" value="Envoyer" />
             </form>
